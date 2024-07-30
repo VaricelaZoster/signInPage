1ebe022aa0b887001f9a3249884546168c25d67d
VaricelaZoster <dwaipayansahastudent2@gmail.com>
2024-07-31 00:05:57 +0530

    removed array, fixed the color of button to button:hover

--- SignIn/script.js
+++ SignIn/script.js
@@ -1,18 +1,18 @@
 let username,password;
 let realpassword = 1234;
-let count = ["Redirecting",1,2,3,4,5];
 let count1 = 5;
 function sub(){
     username = document.getElementById('username').value;
     password = document.getElementById('password').value;
     console.log(username);
     if(password == realpassword){
+        document.getElementById('submit').style.backgroundColor = "rgb(162, 105, 214)";
         document.getElementById('submit').disabled = true;
         document.getElementById('submit').innerHTML = `Ridirecting in.... `;
         setInterval(function(){
-            document.getElementById('submit').innerHTML = `Redirecting in ${count[count1]}`;
+            document.getElementById('submit').innerHTML = `Redirecting in ${count1}`;
             if(count1 == 0){
-                document.getElementById('submit').innerHTML = `${count[count1]}`;
+                document.getElementById('submit').innerHTML = `Redirecting`;
                 location.href = "../SignUpSignIn/index.html";
             }
             count1--;
