6953d3ab28a870a18b44f3710e89fabb7221d00e
VaricelaZoster <[maildwaipayansaha@gmail.com]>
2024-08-02 22:46:32 +0530

    nothing happened today

--- SignUp/script.js
+++ SignUp/script.js
@@ -1,16 +1,23 @@
 let username,password, password1;
-
-let realpassword = 1234;
 let count1 = 3;
+
+
+
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click",sub);
+
 function sub(){
     username = document.getElementById('username').value;
     password = document.getElementById('password').value;
     password1 = document.getElementById('repassword').value;
     console.log(username);
-    if(password == realpassword && password == password1){
+    if(password == password1 && password!= '' && username !=''){
+        console.log(password);
         document.getElementById('submit').style.backgroundColor = "rgb(162, 105, 214)";
         document.getElementById('submit').disabled = true;
         document.getElementById('submit').innerHTML = `Ridirecting in.... `;
+
         setInterval(function(){
             document.getElementById('submit').innerHTML = `Redirecting in ${count1}`;
             if(count1 == 0){
