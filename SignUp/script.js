7ab954e1c02b87db18cce20c807c5c81674175a9
VaricelaZoster <maildwaipayansaha@gmail.com>
2024-08-04 23:18:10 +0530

    idk

--- SignUp/script.js
+++ SignUp/script.js
@@ -1,21 +1,16 @@
-import { connectToDB } from '../con.js';
 
-let username, password, password1;
+document.getElementById('submit').addEventListener("click",submit);
+function submit() {
+    let username, password, password1;
 let count1 = 3;
-
-const submit = document.getElementById("submit");
-submit.addEventListener("click", sub);
-
-function sub() {
     username = document.getElementById('username').value;
     password = document.getElementById('password').value;
     password1 = document.getElementById('repassword').value;
-
     if (password == password1 && password != '' && username != '') {
         document.getElementById('submit').style.backgroundColor = "rgb(162, 105, 214)";
         document.getElementById('submit').disabled = true;
         document.getElementById('submit').innerHTML = "Redirecting in....";
-        connectToDB(username, password);
+        module.exports = {username,password};
         setInterval(function() {
             console.log(count1);
             document.getElementById('submit').innerHTML = `Redirecting in ${count1}`;
--- con.js
+++ con.js
@@ -1,10 +1,5 @@
-
-export function connectToDB(username, password) {
-  console.log(`Username: ${username}, Password: ${password}`);
-  sq(username,password);
-}
-
-function sq(username,password){
+const {username,password} = require('./SignUp/script');
+const {JSDOM} = require('jsdom');
   const _mysql = require('mysql');
   var con = _mysql.createConnection({
     host: "localhost",
@@ -16,11 +11,10 @@ function sq(username,password){
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = `INSERT INTO users (names, pass) VALUES (${username},${password})`;
+    var sql = `INSERT INTO users (names, pass) VALUES (${us.username},${us.password})`;
     con.query(sql,function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
       con.end();
     });
   });
-}
